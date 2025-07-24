302e339f6c3db030c72b491f1ff115208fd6af76
circhastic <achvillanueva20@gmail.com>
2025-07-24 13:51:56 +0800

    fix typo in df

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!")
 st.write(
-  """
+    """
   Choose fruits you want in your custom smoothie!
   """
 )
@@ -18,27 +18,33 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-my_df = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_df = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 # st.dataframe(data=my_df, use_container_width=True)
 
 MAX_INGREDIENTS: int = 6
 ingredients_list = st.multiselect(
     label=f"Choose up to {MAX_INGREDIENTS} ingredients:",
     options=my_df,
-    max_selections=MAX_INGREDIENTS
+    max_selections=MAX_INGREDIENTS,
 )
 
 if ingredients_list:
-    ingredients_string = ''
+    ingredients_string = ""
 
     for chosen_fruit in ingredients_list:
-        ingredients_string += chosen_fruit + ' '
+        ingredients_string += chosen_fruit + " "
 
     # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order +"""')
+    my_insert_stmt = (
+        """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('"""
+        + ingredients_string
+        + """','"""
+        + name_on_order
+        + """')
     """
+    )
 
     # st.write(my_insert_stmt)
 
@@ -46,10 +52,9 @@ if ingredients_list:
 
     if submitted:
         session.sql(my_insert_stmt).collect()
-        st.success(f"Your Smoothie is ordered, {name_on_order}!", icon='✅')
+        st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 smoothiesfroot_response = request.get("https://fruityvice.com/api/fruit/all")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
+sf_df = st.dataframe(data=smoothiesfroot_response.json(), use_container_width=True)
